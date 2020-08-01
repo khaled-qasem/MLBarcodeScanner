fb0e82395c333663755ced4ae0fd072c86c469c3
Khaled Qasem <khaled.alqerem@gmail.com>
2020-08-01 13:28:19 +0300

    Implement camera preview use case

---
+++ app/src/main/java/com/khaled/mlbarcodescanner/CameraXViewModel.kt
@@ -0,0 +1,46 @@
+package com.khaled.mlbarcodescanner
+
+import android.app.Application
+import android.util.Log
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.util.concurrent.ExecutionException
+
+/**
+ * View model for interacting with CameraX.
+ * Create an instance which interacts with the camera service via the given application context.
+ */
+class CameraXViewModel(application: Application) : AndroidViewModel(application) {
+    private var cameraProviderLiveData: MutableLiveData<ProcessCameraProvider>? = null
+
+    // Handle any errors (including cancellation) here.
+    val processCameraProvider: LiveData<ProcessCameraProvider>
+        get() {
+            if (cameraProviderLiveData == null) {
+                cameraProviderLiveData = MutableLiveData()
+                val cameraProviderFuture =
+                    ProcessCameraProvider.getInstance(getApplication())
+                cameraProviderFuture.addListener(
+                    Runnable {
+                        try {
+                            cameraProviderLiveData!!.setValue(cameraProviderFuture.get())
+                        } catch (e: ExecutionException) {
+                            // Handle any errors (including cancellation) here.
+                            Log.e(TAG, "Unhandled exception", e)
+                        } catch (e: InterruptedException) {
+                            Log.e(TAG, "Unhandled exception", e)
+                        }
+                    },
+                    ContextCompat.getMainExecutor(getApplication())
+                )
+            }
+            return cameraProviderLiveData!!
+        }
+
+    companion object {
+        private const val TAG = "CameraXViewModel"
+    }
+}
--- app/src/main/java/com/khaled/mlbarcodescanner/MainActivity.kt
+++ app/src/main/java/com/khaled/mlbarcodescanner/MainActivity.kt
@@ -1,11 +1,142 @@
 package com.khaled.mlbarcodescanner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalStateException
+import kotlin.IllegalStateException
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
+
+    private var previewView: PreviewView? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var cameraSelector: CameraSelector? = null
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+    private var previewUseCase: Preview? = null
+
+    private val screenAspectRatio: Int
+        get() {
+            // Get screen metrics used to setup camera for full screen resolution
+            val metrics = DisplayMetrics().also { previewView?.display?.getRealMetrics(it) }
+            return aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupCamera()
+    }
+
+    private fun setupCamera() {
+        previewView = findViewById(R.id.preview_view)
+        cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+        ViewModelProvider(
+            this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(CameraXViewModel::class.java)
+            .processCameraProvider
+            .observe(this, Observer { provider: ProcessCameraProvider? ->
+                cameraProvider = provider
+                if (isCameraPermissionGranted()) {
+                    bindPreviewUseCase()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.CAMERA),
+                        PERMISSION_CAMERA_REQUEST
+                    )
+                }
+            }
+            )
+    }
+
+
+    private fun bindPreviewUseCase() {
+        if (cameraProvider == null) {
+            return
+        }
+        if (previewUseCase != null) {
+            cameraProvider!!.unbind(previewUseCase)
+        }
+
+        previewUseCase = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(previewView!!.display.rotation)
+            .build()
+        previewUseCase!!.setSurfaceProvider(previewView!!.createSurfaceProvider())
+
+        try {
+            cameraProvider!!.bindToLifecycle(/* lifecycleOwner= */this,
+                cameraSelector!!,
+                previewUseCase
+            )
+        }catch (illegalStateException: IllegalStateException){
+            Log.e(TAG, illegalStateException.message)
+        } catch (illegalArgumentException: IllegalArgumentException){
+            Log.e(TAG, illegalArgumentException.message)
+        }
+    }
+
+    /**
+     *  [androidx.camera.core.ImageAnalysis],[androidx.camera.core.Preview] requires enum value of
+     *  [androidx.camera.core.AspectRatio]. Currently it has values of 4:3 & 16:9.
+     *
+     *  Detecting the most suitable ratio for dimensions provided in @params by counting absolute
+     *  of preview ratio to one of the provided values.
+     *
+     *  @param width - preview width
+     *  @param height - preview height
+     *  @return suitable aspect ratio
+     */
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_CAMERA_REQUEST) {
+            if (isCameraPermissionGranted()) {
+                bindPreviewUseCase()
+            } else {
+                Log.e(TAG, "no camera permission")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun isCameraPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            baseContext,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val PERMISSION_CAMERA_REQUEST = 1
+
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
     }
 }
